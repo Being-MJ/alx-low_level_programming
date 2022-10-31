83e43992399c63ceea1b0437bb86c3b71d667ed3
Maryjane <maryjaneamadi94@gmail.com>
2022-10-31 15:33:31 -0700

    Copied

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - copies memory area
+ * @dest: memory area
+ * @src: memory area src
+ * @n: n bytes we want to change in memory dest
+ *
+ * Retrun: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
