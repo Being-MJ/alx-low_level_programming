a36e4a249bdb8c94522e80da427d6c0d13a52556
Maryjane <maryjaneamadi94@gmail.com>
2022-10-31 15:38:32 -0700

    Copied

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,11 @@
  *
  * Retrun: a pointer to dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
