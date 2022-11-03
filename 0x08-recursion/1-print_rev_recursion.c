7d9776580156761f6e6b99257dc089b4299ea3fa
Maryjane <maryjaneamadi94@gmail.com>
2022-11-02 17:18:12 -0700

    Dare to dream

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  * _print_rev_recursion - prints a string in reverse
  * @s: char pointer of string
  *
- * Return: 0 if successful. 
+ * Return: 0 if successful
  */
 
 void _print_rev_recursion(char *s)
